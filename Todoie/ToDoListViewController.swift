27a4b807862bdc500b594dcedc8b53bba95907c3
Tprezioso <tommyprezioso@gmail.com>
2018-12-05 17:50:28 -0500

    setup app with first tableview

---
+++ Todoie/ToDoListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Todoie
+//
+//  Created by Thomas Prezioso on 12/5/18.
+//  Copyright © 2018 Thomas Prezioso. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    let itemArray = ["egg" ,"cookie", "cheese"]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // MARK - Tableview Datascorce Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+}
+
