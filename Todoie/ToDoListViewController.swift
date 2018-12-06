93241a9176709266b94afef33be83e339ec0c956
Tprezioso <tommyprezioso@gmail.com>
2018-12-06 16:59:15 -0500

    Add checkmark when selected

--- Todoie/ToDoListViewController.swift
+++ Todoie/ToDoListViewController.swift
@@ -31,5 +31,23 @@ class ToDoListViewController: UITableViewController {
         return cell
     }
 
+    // MARK: - Tableview Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print(itemArray[indexPath.row])
+
+        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    
+    
 }
 
