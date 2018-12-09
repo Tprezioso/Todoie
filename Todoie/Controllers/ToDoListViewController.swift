915a934c1ad6d6a2189204d30ebc2547ac05c79e
Tprezioso <tommyprezioso@gmail.com>
2018-12-09 14:40:39 -0500

    added search bar and connected it to core data

--- Todoie/Controllers/ToDoListViewController.swift
+++ Todoie/Controllers/ToDoListViewController.swift
@@ -99,9 +99,9 @@ class ToDoListViewController: UITableViewController {
 
     }
     
-    func loadItems() {
+    // LoadItems method has a internal (with) and External (request) params with default (Item.fetchRequest()) params
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
 
-        let request : NSFetchRequest<Item> = Item.fetchRequest()
         
         do {
             itemArray = try context.fetch(request)
@@ -112,6 +112,30 @@ class ToDoListViewController: UITableViewController {
         
     }
     
-
 }
 
+// MARK: - Search Bar Methods
+
+extension ToDoListViewController : UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+           
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
+
+}
