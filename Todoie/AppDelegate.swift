c465cbf5b4919fb41107e45323844d3783798cfb
Tprezioso <tommyprezioso@gmail.com>
2018-12-11 20:49:08 -0500

    removed unused code

--- Todoie/AppDelegate.swift
+++ Todoie/AppDelegate.swift
@@ -18,14 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
 
-        do {
-            let realm = try Realm()
-
-        } catch {
-            print("Error initialising new realm, \(error)")
-        }
-
-        
         return true
     }
 
