7cb0f10322686419d976c8b9bc7a4bcc06348aef
Tprezioso <tommyprezioso@gmail.com>
2019-01-06 16:01:59 -0500

    still working on local notifications

--- Todoie/AppDelegate.swift
+++ Todoie/AppDelegate.swift
@@ -27,20 +27,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // first, you declare the content of the notification:
         let content = UNMutableNotificationContent()
         content.title = "Notification Title"
-        content.subtitle = "Notification Subtitle"
+        content.sound = UNNotificationSound.default
         content.body = "Notification Body"
         
         // now, you should declare the UNCalendarNotificationTrigger instance,
         // but before that, you'd need to describe what's the date matching for firing it:
         
         // for instance, this means it should get fired every Monday, at 10:30:
-        var date = DateComponents()
-        date.weekday = 6
-        date.hour = 16
-        date.minute = 28
+//        var date = DateComponents()
+//        date.weekday = 6
+//        date.hour = 16
+//        date.minute = 28
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 300, repeats: false)
         
         // declaring the trigger
-        let calendarTrigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
+//        let calendarTrigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
         
         // creating a request and add it to the notification center
         let request = UNNotificationRequest(identifier: "notification-identifier", content: content, trigger: calendarTrigger)
